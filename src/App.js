bd3be5fbffc4b450a63cf9c2c6d570ea43e40545
Ikeh Leonard <leoikeh99@gmail.com>
2021-05-10 12:17:29 +0100

    completed animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 import WhatIdo from "./components/WhatIdo";
